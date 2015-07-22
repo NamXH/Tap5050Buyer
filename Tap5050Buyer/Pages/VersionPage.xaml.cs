27e47b23968480d8df45514b0d6b920e3352e8c1
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-07-21 21:31:43 -0600

    Add version page

---
+++ Tap5050Buyer/Pages/VersionPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Tap5050Buyer
+{
+    public partial class VersionPage : ContentPage 
+    {
+        public VersionPage()
+        {
+            InitializeComponent();
+            Title = "Version0.1";
+        }
+    }
+}
+
