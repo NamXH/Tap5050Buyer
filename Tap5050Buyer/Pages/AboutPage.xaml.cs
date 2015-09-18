ed85558cfcd3a52fa2e66205aabc8d0293c619dc
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-09-18 14:21:55 +0700

    Change version number

--- Tap5050Buyer/Pages/AboutPage.xaml.cs
+++ Tap5050Buyer/Pages/AboutPage.xaml.cs
@@ -10,7 +10,7 @@ namespace Tap5050Buyer
         public AboutPage()
         {
             InitializeComponent();
-            Title = "Buy Tickets v1.2";
+            Title = "Buy Tickets v1.3";
 
             var scroll = new ScrollView();
             Content = scroll;
